6d1d6c7c49849a12dd3b4cc3bf8d83f582440685
Ed Schofield <ed@pythoncharmers.com>
2017-08-03 12:05:54 +1000

    Update notebook 2

--- Notebooks/solutions/check_normalized.py
+++
@@ -1,3 +0,0 @@
-
-assert math.isclose(1, sum(posterior(model, 'spots') for model in ['smallpox', 'not smallpox']))
-assert math.isclose(1, sum(posterior(model, 'no spots') for model in ['smallpox', 'not smallpox']))
